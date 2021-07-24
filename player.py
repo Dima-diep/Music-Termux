ec34474fe6b3230456e8fb8467b7fef71542c929
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-24 17:11:05 +0400

    player.py

---
+++ player.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+import os
+from termcolor import colored
+from colorama import init
+
+init()
+
+os.system("clear")
+print(colored('_________________________', 'blue', 'on_red'))
+print(colored('|                       |', 'blue', 'on_red'))
+print(colored('|      -------------    |', 'blue', 'on_red'))
+print(colored('|     |            |    |', 'blue', 'on_red'))
+print(colored('|     |            |    |', 'blue', 'on_red'))
+print(colored('|     |            |    |', 'blue', 'on_red'))
+print(colored('|     |            |    |', 'blue', 'on_red'))
+print(colored('|   JJL            |    |', 'blue', 'on_red'))
+print(colored('|  JJJJ           JJ    |', 'blue', 'on_red'))
+print(colored('|  JJJJ          JJJ    |', 'blue', 'on_red'))
+print(colored('|   JJ            JJ    |', 'blue', 'on_red'))
+print(colored('-------------------------', 'blue', 'on_red'))
+print('------------------------')
+a = input('Your Music: ')
+print('------------------------')
+os.system('mpv ' + a + ' > /dev/null')
+print('------------------------')
